aa31d35113b50140e382b951965df1e5391d47fc
Danny Ozuna Vasquez <69773592+DannyOzuna@users.noreply.github.com>
2023-03-20 09:54:17 -0400

    feat: add Logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FeaturesComponent } from './features/features.component';
 
 const routes: Routes = [
   {
